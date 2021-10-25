a9fc8fbfe92a4757c560a53e4809d5d7cdb38ba1
horoshopodumaj <murka8831@yandex.ru>
2021-10-25 22:21:05 +0300

    add solution

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,23 @@
 let calculator = {
-  // ваш код
+
+  firstNumber: a,
+  secondNumber: b,
+  
+  read(a, b) {
+    // this.firstNumber = a;
+    // this.secondNumber = b;
+    return a, b;
+    // this.firstNumber = +prompt("Введите первое число", 0);
+    // this.secondNumber = +prompt("Введите второе число", 0);
+  },
+ 
+  sum() {
+    return this.firstNumber + this.secondNumber;
+  },
+
+  mul() {
+    return this.firstNumber * this.secondNumber;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
