a4ae0f49fc92f8590790720ff416f826184bb2b1
horoshopodumaj <murka8831@yandex.ru>
2021-10-28 21:22:19 +0300

    add solution for module_4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,13 +1,13 @@
 function showSalary(users, age) {
+  
   let userArray = []
   let someUsers = users.filter(function(user) {
+
     if (user.age <= age) {
-      
       userArray.push(user.name, ', ', user.balance, '\n');
-  
     }
   });
   
   let result = userArray.join('')
-  return result;
+  return result.trim();
 }
